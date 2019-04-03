c0fa0430a4a0583db7f3d4c12831f2f1181615c1
fwiffo <fwiffo@fwiffo.net>
2019-04-03 15:31:33 +0200

    missing bat file

---
+++ install.bat
@@ -0,0 +1,34 @@
+@echo off
+
+cd %APPVEYOR_BUILD_FOLDER%
+
+echo Compiler: %COMPILER%
+echo Architecture: %MSYS2_ARCH%
+echo Platform: %PLATFORM%
+echo MSYS2 directory: %MSYS2_DIR%
+echo MSYS2 system: %MSYSTEM%
+echo Bits: %BIT%
+
+REM Create a writeable TMPDIR
+mkdir %APPVEYOR_BUILD_FOLDER%\tmp
+set TMPDIR=%APPVEYOR_BUILD_FOLDER%\tmp
+
+IF %COMPILER%==msys2 (
+  @echo on
+  SET "PATH=C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%"
+  bash -lc "pacman -S --needed --noconfirm pacman-mirrors"
+  bash -lc "pacman -S --needed --noconfirm git"
+  REM Update
+  bash -lc "pacman -Syu --noconfirm"
+
+  REM build tools
+  bash -lc "pacman -S --needed --noconfirm mingw-w64-x86_64-toolchain autoconf automake libtool make patch mingw-w64-x86_64-libtool"
+
+  REM Native deps
+  bash -lc "pacman -S --needed --noconfirm mingw-w64-x86_64-gobject-introspection mingw-w64-x86_64-cairo mingw-w64-x86_64-gtk3 mingw-w64-x86_64-expat mingw-w64-x86_64-openssl"
+
+  REM There is not a corresponding cc for the mingw64 gcc. So we copy it in place.
+  bash -lc "cp -pv /mingw64/bin/gcc /mingw64/bin/cc"
+
+  bash -lc "make"
+)
